37b6da96f3b1de38a24a6a2c698affc0a3b440af
Robert Clausecker <fuz@fuz.su>
2024-08-30 11:24:05 +0200

    hwcap_generic.c: add generic do-noting fallback

---
+++ hwcap_generic.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+
+#include "hwcap.h"
+
+void
+caps_from_auxv(void)
+{
+}
+
+void
+caps_all(void)
+{
+}
+
+void
+print_cflags(void)
+{
+}
+
+const struct cap *
+get_archlevel(void)
+{
+
+	return (NULL);
+}
